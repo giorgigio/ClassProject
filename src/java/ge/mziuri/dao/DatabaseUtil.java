2901436645ec1a7b83aef4b8e6428450271ef319
Giorgi <giorgigio740@gmail.com>
2017-02-19 19:55:46 +0400

    initial commit...

---
+++ src/java/ge/mziuri/dao/DatabaseUtil.java
@@ -0,0 +1,33 @@
+package ge.mziuri.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseUtil {
+    
+    public static final String databaseURL = "jdbc:postgresql://localhost:5432/ClassProject";
+    
+    public static final String username = "postgres";
+    
+    public static final String passsword = "rame";
+    
+    public static Connection getConnection() {
+        Connection con = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            con = DriverManager.getConnection(databaseURL, username, passsword);
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return con;
+    }
+    
+    public static void closeConnection(Connection con) {
+        try {
+            con.close();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
---
+++ src/java/ge/mziuri/servlet/LoginServlet.java
@@ -0,0 +1,5 @@
+package ge.mziuri.servlet;
+
+public class LoginServlet {
+    
+}
