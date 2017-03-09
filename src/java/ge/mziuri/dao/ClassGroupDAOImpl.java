a053049c054e22fa2cde8e59d1f74a429049ca20
Giorgi Mikeladze <giorgigio740@gmail.com>
2017-03-09 18:11:24 +0400

    modified ClassGroupDAOImpl...

--- src/java/ge/mziuri/dao/ClassGroupDAOImpl.java
+++ src/java/ge/mziuri/dao/ClassGroupDAOImpl.java
@@ -18,7 +18,22 @@ public class ClassGroupDAOImpl implements ClassGroupDAO {
 
     @Override
     public void addClassGroup(ClassGroup classgroup) {
-        
+        try {
+            String sql = "INSERT INTO system_user (name) VALUES (?) RETURNING id";
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, classgroup.getName());
+            ResultSet rs = pstmt.executeQuery();
+            rs.next();
+            int groupId = rs.getInt("id");
+            String userSql = "UPDATE system_user SET created_group_id = ?, joined_group_id = ? WHERE id = ?";
+            pstmt = conn.prepareCall(userSql);
+            pstmt.setInt(1, groupId);
+            pstmt.setInt(2, groupId);
+            pstmt.setInt(3, classgroup.getCreator().getId());
+            pstmt.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 
     @Override
